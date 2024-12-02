09b6ea92820cf842f8822f95f574bbc4b152b740
Moataz Chaabane <chaabanemoataz@outlook.fr>
2024-12-02 23:11:41 +0100

    cleaning main branch

--- router/history.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const HistCrtl = require("../controllers/history");
-
-router.get("/:id_place",HistCrtl.gethistbyplace);
-router.get("/",HistCrtl.gethist);
-router.get("/:id_place/:date",HistCrtl.gethistbydate);
-
-module.exports = router;
--- router/permission.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PerCtrl = require("../controllers/permission");
-
-// router.post("/",PerCtrl.createPer);
-
-router.get("/",PerCtrl.getAllPer);
-
-module.exports = router; 
